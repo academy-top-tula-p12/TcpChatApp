df7d1a1679eb676a462666255a79e894fafc6281
Asteron2007 <52139280+Asteron2007@users.noreply.github.com>
2024-08-24 11:39:53 +0300

    Добавьте файлы проекта.

---
+++ TcpChatApp/TcpChatClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpChatListenerApp
+{
+    public class TcpChatClient
+    {
+        TcpClient client;
+        TcpChatServer server;
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string Name { get; set; }
+        public StreamWriter Writer { get; }
+        public StreamReader Reader { get; }
+
+        public TcpChatClient(string name, TcpClient client, TcpChatServer server)
+        {
+            this.client = client;
+            this.server = server;
+            Name = name;
+
+            var stream = client.GetStream();
+            Writer = new(stream);
+            Reader = new(stream);
+        }
+
+        public async Task ProcessAsync()
+        {
+
+        }
+
+        public void Close()
+        {
+
+        }
+    }
+}
---
+++ TcpChatApp/TcpChatServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpChatListenerApp
+{
+    public class TcpChatServer
+    {
+        IPAddress address = IPAddress.Loopback;
+        int port = 5000;
+        TcpListener listener;
+        List<TcpChatClient> clients;
+
+        public TcpChatServer()
+        {
+            listener = new TcpListener(new IPEndPoint(address, port));
+            clients = new List<TcpChatClient>();
+        }
+
+        public async Task ListenAsync()
+        {
+
+        }
+
+        public async Task MessageSendAsync(string id, string message)
+        {
+
+        }
+
+        public void ClientClose(string id)
+        {
+
+        }
+
+        public void ClientsAllClose()
+        {
+
+        }
+    }
+}
